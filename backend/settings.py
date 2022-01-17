a7c6f5e71ffd3cf832ee74262584b3198ea34766
Rishi Namdev <namdev.rishi.developer@gmail.com>
2022-01-18 02:10:31 +0530

    new changes that makes production swagger functional

--- backend/settings.py
+++ backend/settings.py
@@ -22,7 +22,7 @@ MIDDLEWARE+=[
 REST_FRAMEWORK = {
     # Use Django's standard `django.contrib.auth` permissions,
     # or allow read-only access for unauthenticated users.
-        'DEFAULT_SCHEMA_CLASS': 'rest_framework.schemas.coreapi.AutoS   chema',
+        'DEFAULT_SCHEMA_CLASS': 'rest_framework.schemas.coreapi.AutoSchema',
     'DEFAULT_PERMISSION_CLASSES': [
         'rest_framework.permissions.DjangoModelPermissionsOrAnonReadOnly'
     ]
